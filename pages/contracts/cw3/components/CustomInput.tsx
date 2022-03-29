ec04c5511f8ab4a35d265ee0e0fd33cab36203d2
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-03-29 10:46:59 +0300

    add tooltip and make listing two columns

--- pages/contracts/cw3/components/CustomInput.tsx
+++ pages/contracts/cw3/components/CustomInput.tsx
@@ -39,40 +39,40 @@ const CustomInput = (props: {
   }
 
   return (
-    <div className='px-3'>
-      <div className='flex flex-row'>
-        <div className='mb-6 w-9/12'>
+    <div className="px-3">
+      <div className="flex flex-row">
+        <div className="mb-6 w-9/12">
           <label
-            htmlFor='small-input'
-            className='block mb-2 text-md font-medium text-gray-900 dark:text-gray-300'
+            htmlFor="small-input"
+            className="block mb-2 text-md font-medium text-gray-900 dark:text-gray-300"
           >
             {props.placeholder}
           </label>
           <input
-            type='text'
-            className='rounded py-2 px-1 text-black w-full'
+            type="text"
+            className="rounded py-2 px-1 text-black w-full"
             value={input}
             onChange={handleChange}
             placeholder={props.placeholder}
           />
         </div>
         <button
-          type='button'
-          className=' basis-1/12 mt-2 ml-1'
+          type="button"
+          className=" basis-1/12 mt-2 ml-1"
           onClick={addClicked}
         >
-          <span className='hover:text-juno p-2 border-2 rounded-full'>+</span>
+          <span className="hover:text-juno p-2 border-2 rounded-full">+</span>
         </button>
       </div>
-      <div className='grid grid-cols-2 w-3/4'>
+      <div className="grid grid-cols-2 w-3/4">
         {items.map((item: string) => {
           return (
             <div key={item}>
-              <div className='col-span-1 grid grid-cols-2 p-2 rounded bg-black bg-opacity-10'>
+              <div className="col-span-1 grid grid-cols-2 p-2 rounded bg-black bg-opacity-10">
                 <button
-                  type='button'
+                  type="button"
                   onClick={() => copy(item)}
-                  className='text-sm'
+                  className="text-sm"
                   data-bs-toggle="tooltip"
                   title={item}
                   // title="<em>Tooltip</em> <u>with</u> <b>HTML</b>"
@@ -83,12 +83,12 @@ const CustomInput = (props: {
                     item.slice(item.length - 5, item.length)}
                 </button>
                 <div
-                  className='ml-5'
+                  className="ml-5"
                   onClick={() => {
                     removeClicked(item)
                   }}
                 >
-                  <button type='button' className='hover:text-juno pl-2'>
+                  <button type="button" className="hover:text-juno pl-2">
                     x
                   </button>
                 </div>
