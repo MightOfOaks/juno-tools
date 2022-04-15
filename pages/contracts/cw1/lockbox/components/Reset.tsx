c547ed798f0989d7cfc6df08775c7fb0d04cc4ce
eray <eray.keceli16@gmail.com>
2022-04-15 09:06:52 +0300

    Tooltips removed

--- pages/contracts/cw1/lockbox/components/Reset.tsx
+++ pages/contracts/cw1/lockbox/components/Reset.tsx
@@ -5,8 +5,6 @@ import React, { useState } from 'react'
 import toast from 'react-hot-toast'
 import { FaAsterisk } from 'react-icons/fa'
 
-import Tooltip from '../../../../../components/Tooltip'
-
 const Reset = (props: { contractAddress: string }) => {
   const { contractAddress } = props
 
@@ -90,20 +88,6 @@ const Reset = (props: { contractAddress: string }) => {
           >
             Lockbox ID
           </label>
-          <Tooltip label="The minimum timelock duration for future operations.">
-            <svg
-              className="mt-1 w-4 h-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </Tooltip>
         </div>
 
         <div className="flex mt-3">
